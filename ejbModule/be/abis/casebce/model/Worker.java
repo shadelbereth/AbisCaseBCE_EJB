99a894521bcd9d5fcee668f92408d12a2318972b
Abdelilah <abdelilah.razzouk@student.ehb.be>
2019-09-04 10:44:54 +0200

    Worker transformed into entity

--- ejbModule/be/abis/casebce/model/Worker.java
+++ ejbModule/be/abis/casebce/model/Worker.java
@@ -5,19 +5,33 @@ import java.io.Serializable;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "Person")
 @Named
 @SessionScoped
-public class Worker implements Serializable{
+public class Worker implements Serializable {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "id")
+	private int perfermorId;
+	@Column(name = "login")
 	private String login;
-	private String password;
-	private String lastName;
+	@Column(name = "firstName")
 	private String firstName;
+	@Column(name = "lastName")
+	private String lastName;
 	private String bankAcount;
 	@Inject
 	private Address address;
-	
-	
+	private String password;
+
 	public String getLogin() {
 		return login;
 	}
@@ -65,9 +79,13 @@ public class Worker implements Serializable{
 	public void setAddress(Address address) {
 		this.address = address;
 	}
-	
-	
-	
 
-	
+	public int getPerfermorId() {
+		return perfermorId;
+	}
+
+	public void setPerfermorId(int perfermorId) {
+		this.perfermorId = perfermorId;
+	}
+
 }
