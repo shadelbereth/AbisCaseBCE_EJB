e7cd5405a47cc42f9f4b5480c3d0d55654171160
Sophie Roch <scallakim@hotmail.com>
2019-09-05 10:41:38 +0200

    Add comment

--- CreateSequence.sql
+++ CreateSequence.sql
@@ -1,6 +1,6 @@
 --<ScriptOptions statementTerminator=";"/>
 
-CREATE SEQUENCE Company_Seq
+CREATE SEQUENCE Company_Seq -- replace with your class name --
      MINVALUE 1
      START WITH 1
      INCREMENT BY 1
