1176c7b9cf6312821e0f33882dce914906a39423
Abdelilah <abdelilah.razzouk@student.ehb.be>
2019-09-05 15:35:14 +0200

    fixed

--- ejbModule/be/abis/casebce/model/Worker.java
+++ ejbModule/be/abis/casebce/model/Worker.java
@@ -20,72 +20,42 @@ public class Worker implements Serializable {
 	@Id
 	@GeneratedValue
 	@Column(name = "id")
-	private int perfermorId;
+	private int performerId;
 	@Column(name = "login")
 	private String login;
 	@Column(name = "firstName")
 	private String firstName;
 	@Column(name = "lastName")
 	private String lastName;
+	/*
 	private String bankAcount;
 	@Inject
 	private Address address;
 	private String password;
-
+*/
+	public int getPerformerId() {
+		return performerId;
+	}
+	public void setPerformerId(int performerId) {
+		this.performerId = performerId;
+	}
 	public String getLogin() {
 		return login;
 	}
-
 	public void setLogin(String login) {
 		this.login = login;
 	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
 	public String getFirstName() {
 		return firstName;
 	}
-
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-
-	public String getBankAcount() {
-		return bankAcount;
+	public String getLastName() {
+		return lastName;
 	}
-
-	public void setBankAcount(String bankAcount) {
-		this.bankAcount = bankAcount;
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
 	}
-
-	public Address getAddress() {
-		return address;
-	}
-
-	public void setAddress(Address address) {
-		this.address = address;
-	}
-
-	public int getPerfermorId() {
-		return perfermorId;
-	}
-
-	public void setPerfermorId(int perfermorId) {
-		this.perfermorId = perfermorId;
-	}
-
+	
 }
