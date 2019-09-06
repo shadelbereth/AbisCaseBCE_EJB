86a0d1358ff9166168de655a93ca2f8d55b11ce6
chris <christiansandjong@hotmail.com>
2019-09-06 09:15:26 +0200

    test activity jpa with db

--- ejbModule/be/abis/casebce/model/Activity.java
+++ ejbModule/be/abis/casebce/model/Activity.java
@@ -13,8 +13,12 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Persistence;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
@@ -24,19 +28,43 @@ import javax.persistence.Table;
 public class Activity implements Serializable {
 
 	@Id
-	@GeneratedValue
+	@SequenceGenerator(name = "Activity_Gen", sequenceName = "Activity_Seq", allocationSize=1)
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator = "Activity_Gen")
 	@Column(name = "id")
 	private int activityId;
-	@Column(name = "start_time")
+	@Column(name = "start_time", columnDefinition = "TIMESTAMP")
 	private LocalDateTime start;
-	@Column(name = "end_time")
+	@Column(name = "end_time" ,columnDefinition = "TIMESTAMP")
 	private LocalDateTime end;
 	@Column(name = "description")
 	private String description;
 
 	@Inject
+	@ManyToOne 
+	@JoinColumn(name = "projectid")
 	private Project project;
+	@Inject
+	@ManyToOne
+	@JoinColumn(name = "performerid")
 	private Worker performer;
+	
+	// constructor 
+	
+	
+	public Activity() {
+		super();
+	}
+	
+	public Activity(LocalDateTime start, LocalDateTime end, String description, Project project, Worker performer) {
+		super();
+		this.start = start;
+		this.end = end;
+		this.description = description;
+		this.project = project;
+		this.performer = performer;
+	}
+
+	
 
 	// getter and setters
 
