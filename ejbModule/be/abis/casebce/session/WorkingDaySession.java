02a4b6e85818affcd51cfd3b68e73976cf633bac
chris <christiansandjong@hotmail.com>
2019-09-13 12:03:19 +0200

    create WorkingDay JPA

--- ejbModule/be/abis/casebce/session/WorkingDaySession.java
+++ ejbModule/be/abis/casebce/session/WorkingDaySession.java
@@ -1,11 +1,14 @@
 package be.abis.casebce.session;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
+import be.abis.casebce.model.Activity;
 import be.abis.casebce.model.ExternalWorker;
 import be.abis.casebce.model.Worker;
 import be.abis.casebce.model.WorkingDay;
@@ -18,27 +21,40 @@ public class WorkingDaySession implements WorkingDaySessionRemote {
 
 	@PersistenceContext(unitName = "CaseBCE")
 	private EntityManager em;
+
 	/**
 	 * Default constructor.
 	 */
 	public WorkingDaySession() {
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
+
 	public WorkingDay getCurrentWorkingDay(ExternalWorker worker) {
+
 		// get last working day from db
+		List<WorkingDay> days = em.createQuery("SELECT w FROM WorkingDay w order by w.end desc", WorkingDay.class)
+				.setMaxResults(1).getResultList();
+
+		WorkingDay day = days.get(0);
+
 		// if it is open return it
-		// else create new one
-		WorkingDay day = new WorkingDay();
-		day.setWorker(worker);
-		return day;
+		if (day.getEnd() == null) {
+			day.setWorker(worker);
+			return day;
+
+		} else {
+
+			// else create new one
+			day = new WorkingDay();
+			day.setWorker(worker);
+			return day;
+		}
 	}
 
 	@Override
 	public WorkingDay startWorkingDay(WorkingDay workingDay) {
 		workingDay.setStart(LocalDateTime.now());
-		// persist into DB
 		return workingDay;
 	}
 
@@ -46,6 +62,8 @@ public class WorkingDaySession implements WorkingDaySessionRemote {
 	public WorkingDay closeWorkingDay(WorkingDay workingDay) {
 		workingDay.setEnd(LocalDateTime.now());
 		// merge into DB
+		em.merge(workingDay.getWorker());
+		em.persist(workingDay);
 		// create new one to fill front end
 		WorkingDay day = new WorkingDay();
 		day.setWorker(workingDay.getWorker());
---
+++ seqcreate.sql
@@ -0,0 +1,12 @@
+CREATE SEQUENCE Activity_Seq -- replace with your class name --
+     MINVALUE 1
+     START WITH 1
+     INCREMENT BY 1
+     ;
+     
+     CREATE SEQUENCE WorkingDay_Seq -- replace with your class name --
+     MINVALUE 1
+     START WITH 1
+     INCREMENT BY 1;
+     
+     show ;
