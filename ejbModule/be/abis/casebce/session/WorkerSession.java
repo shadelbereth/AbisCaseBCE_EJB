c538a6ac79776dc68ecd9dfeed5881852893fa5a
Abdelilah <abdelilah.razzouk@student.ehb.be>
2019-09-17 12:27:05 +0200

    hashing check added

--- ejbModule/be/abis/casebce/session/WorkerSession.java
+++ ejbModule/be/abis/casebce/session/WorkerSession.java
@@ -1,9 +1,11 @@
 package be.abis.casebce.session;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.ws.rs.WebApplicationException;
 
 import be.abis.casebce.model.Worker;
 
@@ -34,8 +36,25 @@ public class WorkerSession implements WorkerSessionRemote {
 	public Worker login(String login, String password) {
 		Worker worker = em
 				.createQuery("Select p From Worker p Where p.login = :login AND p.password = :password", Worker.class)
-				.setParameter("password", password).setParameter("login", login).getSingleResult();
+				.setParameter("password", hash(password)).setParameter("login", login).getSingleResult();
 		return worker;
+	}
+
+	private static String hash(String text) {
+		MessageDigest md = null;
+		try {
+			md = MessageDigest.getInstance("SHA-512");
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		md.update(text.getBytes());
+		byte[] digest = md.digest();
+		String myHash = "";
+		for (byte b : digest) {
+			myHash += String.format("%02X", b);
+		}
+
+		return myHash.toLowerCase();
 	}
 
 }
